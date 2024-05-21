533012d7b4b466968ce9d26a5d5dc5d9f795a199
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-05-20 23:27:42 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int scoreOfString(char* s) {
+    int combo = 0;
+    int firstVal = 0;
+    int secondVal = 0;
+    for (int x = 0; x < strlen(s); x++)
+    {
+        if(x+1 < strlen(s))
+        {
+            firstVal = s[x];
+            secondVal = s[x+1];
+        }
+        if((firstVal-secondVal) > 0)
+        {
+            combo += firstVal - secondVal;
+        }
+        if((secondVal-firstVal) > 0)
+        {
+            combo += secondVal - firstVal;
+        }
+//        printf("firstval = %d, secondVal = %d, combo = %d\n",firstVal,secondVal,combo);
+        firstVal = 0;
+        secondVal = 0;
+    }
+    return combo;
+}
